71772de37a27226a13d09dec1d4bdc4b6780de81
MisterPurl <romain.lajeunesse9@gmail.com>
2020-07-15 15:51:20 +0200

    maj post_install

--- post_install.sh
+++ post_install.sh
@@ -3,4 +3,13 @@ sudo apt update -y
 sudo apt install docker docker.io -y
 systemctl enable docker
 service docker start
-docker run --name nginx -p 80:80 -d nginx
+mkdir /home/html && touch /home/html/index.html
+cat <<EOF > /home/html/index.html
+<html>
+<body>
+<h1>Hello Lajeunesse Romain</h1>
+<p>hostname is: $(hostname)</p>
+</body>
+</html>
+EOF
+docker run --name nginx -p 80:80 -v /home/html:/usr/share/nginx/html -d nginx
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "instance_type" {
     type = string
-    default = "t2.nano"
+    default = "t2.micro"
 }
