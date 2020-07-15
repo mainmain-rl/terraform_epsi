24f971ecaef09af6992cb07d73640ae52ebd377e
MisterPurl <romain.lajeunesse9@gmail.com>
2020-07-15 14:15:46 +0200

    update 1507

--- main.tf
+++ main.tf
@@ -110,37 +110,7 @@ resource "aws_instance" "ec2-test-2" {
   }
 }
 
-#Create LOADBALANCER
-resource "aws_elb" "elb_terraform" {
-  availability_zones = ["us-east-1"]
-  security_groups   = [aws_security_group.allow_http.id]
-  subnets           = [aws_subnet.subnet_1_terraform.id]
 
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
-    target              = "HTTP:80/"
-    interval            = 30
-  }
-
-  instances                   = [aws_instance.ec2-test-1.id, aws_instance.ec2-test-2.id]
-  cross_zone_load_balancing   = true
-  idle_timeout                = 400
-  connection_draining         = true
-  connection_draining_timeout = 400
-
-  tags = {
-    Name = "elb_terraform"
-  }
-}
 #################### END INSTANCE ####################
 
 #################### SECURITY GROUP ####################
