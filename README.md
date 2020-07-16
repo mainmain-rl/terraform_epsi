a8112404013c021bd98b41c5cde486493bf30534
MisterPurl <romain.lajeunesse9@gmail.com>
2020-07-16 16:28:50 +0200

    commit test final lb

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # terraform_epsi
 ![Terraform Pull Request](https://github.com/MisterPurl/terraform_epsi/workflows/Terraform%20Pull%20Request/badge.svg)
-demo2
--- main.tf
+++ main.tf
@@ -8,8 +8,7 @@ provider "aws" {
 resource "aws_vpc" "vpc_terraform" {
   cidr_block = "10.10.0.0/16"
   tags = {
-    Name = "vpc_terraform"
-    Env = "tp"
+    name = "vpc_terraform"
   }
 }
 
@@ -17,49 +16,23 @@ resource "aws_vpc" "vpc_terraform" {
 resource "aws_subnet" "subnet_1_terraform" {
   cidr_block = "10.10.1.0/24"
   vpc_id = aws_vpc.vpc_terraform.id
-  
   tags = {
     Name = "subnet_1_terraform"
-    Env = "tp"
    }
-  }
-  resource "aws_subnet" "subnet_2_terraform" {
+}
+resource "aws_subnet" "subnet_2_terraform" {
   cidr_block = "10.10.2.0/24"
   vpc_id = aws_vpc.vpc_terraform.id
-  
   tags = {
     Name = "subnet_2_terraform"
-    Env = "tp"
    }
-  }
-
-resource "aws_subnet" "subnet_3_terraform" {
-  cidr_block = "10.10.3.0/24"
-  vpc_id = aws_vpc.vpc_terraform.id
-  
-  tags = {
-    Name = "subnet_3_terraform"
-    Env = "tp"
-   }
-  }
-resource "aws_subnet" "subnet_4_terraform" {
-
-  cidr_block = "10.10.4.0/24"
-  vpc_id = aws_vpc.vpc_terraform.id
-  
-  tags = {
-    Name = "subnet_4_terraform"
-    Env = "tp"
-   }
-  }
-
+}
   # Create gateway
   resource "aws_internet_gateway" "internetgateway_terraform" {
   vpc_id = aws_vpc.vpc_terraform.id
 
   tags = {
     Name = "internetgateway_terraform"
-    Env = "tp"
   }
 }
 
@@ -74,10 +47,8 @@ resource "aws_route_table" "routetable_terraform" {
 
   tags = {
     Name = "routetable_terraform"
-    Env = "tp"
   }
 }
-
 #Create association route table
 resource "aws_route_table_association" "routetableassociation" {
   subnet_id      = aws_subnet.subnet_1_terraform.id
@@ -97,7 +68,7 @@ resource "aws_key_pair" "ec2-key-tf" {
 
 ######################################## INSTANCE ########################################
 #Create EC2
-data "aws_ami" "ubuntu" {
+/*data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
@@ -138,7 +109,86 @@ resource "aws_instance" "ec2-test-2" {
   tags = {
     Name = "ec2-test-2"
   }
+}*/
+
+##### LB #####
+#Create loadbalancer
+resource "aws_lb" "alb_terraform" {
+  name               = "alb_terraform"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.allow_http.id]
+  subnets            = ["aws_subnet.subnet_1_terraform.id", "aws_subnet.subnet_2_terraform.id"]
 }
+#Create lb target group
+resource "aws_lb_target_group" "lb_target_group_tf" {
+  name     = "lb_target_group_tf"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc_terraform.id
+}
+#Create lb listner
+resource "aws_lb_listener" "alb_listner_terraform" {
+  load_balancer_arn = aws_lb.alb_terraform.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lb_target_group_tf.arn
+  }
+}
+#creation lb target groupe attachment
+resource "aws_lb_target_group_attachment" "target_group_attachment_tf" {
+  target_group_arn = aws_lb_target_group.lb_target_group_tf.arn
+  target_id        = aws_instance.lb_target_group_tf.id
+  port             = 80
+}
+##### END LB #####
+
+##### ASG #####
+#Auto-scalling-group
+resource "aws_placement_group" "asg_placement_group_terraform" {
+  name     = "asg_placement_group_terraform"
+  strategy = "cluster"
+}
+
+resource "aws_autoscaling_group" "asg_terraform" {
+  name                      = "asg_terraform"
+  max_size                  = 3
+  min_size                  = 2
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 4
+  force_delete              = true
+  placement_group           = aws_placement_group.asg_placement_group_terraform.id
+  launch_configuration      = aws_launch_configuration.launch_configuration_terraform.name
+  vpc_zone_identifier       = aws_subnet.subnet_1_terraform.id
+
+  initial_lifecycle_hook {
+    name                 = "asg_lifecycle_terraform"
+    default_result       = "CONTINUE"
+    heartbeat_timeout    = 2000
+    lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
+  }
+
+  timeouts {
+    delete = "5m"
+  }
+}
+
+resource "aws_autoscaling_attachment" "asg_attachment_terraform" {
+  autoscaling_group_name = aws_autoscaling_group.asg_terraform.id
+  alb_target_group_arn   = aws_alb_target_group.lb_target_group_tf.arn
+}
+#Create LAUNCH CONFIGURATION
+resource "aws_launch_configuration" "launch_configuration_terraform" {
+  image_id = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  security_groups = aws_security_group.allow_http.id
+  user_data = file("${path.module}/post_install.sh")
+}
+##### END ASG #####
 ######################################## END INSTANCE ########################################
 
 ######################################## SECURITY GROUP ########################################
