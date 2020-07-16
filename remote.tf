24226ce6381a7bdf23c25493bcab97c89d07e6a1
MisterPurl <romain.lajeunesse9@gmail.com>
2020-07-16 09:54:33 +0200

    test 10 credentials de merde fuck sans bucket

--- remote.tf
+++ remote.tf
@@ -1,7 +1,7 @@
-terraform {
-    backend "s3" {
-        bucket = "epsi-rlajeunesse"
-        key = "terraform_tfstate"
-        region = "us-east-1"
-    }
-}
+#terraform {
+#    backend "s3" {
+#        bucket = "epsi-rlajeunesse"
+#        key = "terraform_tfstate"
+#        region = "us-east-1"
+#    }
+#}
